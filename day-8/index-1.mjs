b9b423eeec2de91c41231b1b45405ccccb4137ed
Adam hakansson <adam97.live.com@live.com>
2023-12-08 17:42:03 +0100

    Fix bug for windows

--- day-8/index-1.mjs
+++ day-8/index-1.mjs
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 
-const data = (await fs.readFile("data", "utf-8")).trim().split("\n");
+const data = (await fs.readFile("data", "utf-8")).trim().split("\r\n");
 
 const [moves, nodes] = data.reduce(
   (acc, line, index) => {
--- day-8/index-2.mjs
+++ day-8/index-2.mjs
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 
-const data = (await fs.readFile("data", "utf-8")).trim().split("\n");
+const data = (await fs.readFile("data", "utf-8")).trim().split("\r\n");
 
 const [moves, nodes, players] = data.reduce(
   (acc, line, index) => {
