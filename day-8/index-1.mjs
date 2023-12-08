7a6452c94a480b784c39ea0521d8dd82858ac0dc
AdamH <adam97.live.com@live.com>
2023-12-08 09:25:35 +0100

    Day-8 part-1 complete

---
+++ day-8/index-1.mjs
@@ -0,0 +1,33 @@
+import fs from "fs/promises";
+
+const data = (await fs.readFile("data", "utf-8")).trim().split("\n");
+
+const [moves, nodes] = data.reduce(
+  (acc, line, index) => {
+    if (index === 0) return [line, []];
+    const [nodeName, nodeLeft, nodeRight] = [...line.matchAll(/\w+/g)].map(String);
+    if (!nodeName) return [acc[0], acc[1]];
+    return [acc[0], { ...acc[1], [nodeName]: { L: nodeLeft, R: nodeRight } }];
+  },
+  [[], {}]
+);
+
+let movesTaken = 1;
+const queue = [{ nextMove: nodes.AAA[moves[0]] }];
+while (queue.length > 0) {
+  const nextMove = moves[movesTaken++ % moves.length];
+
+  const { nextMove: nextNode } = queue.shift();
+
+  if (nextNode === "ZZZ") {
+    break;
+  }
+
+  if (nextMove === "L") {
+    queue.push({ nextMove: nodes[nextNode].L });
+  } else if (nextMove === "R") {
+    queue.push({ nextMove: nodes[nextNode].R });
+  }
+}
+
+console.log("Part 1:", movesTaken - 1);
